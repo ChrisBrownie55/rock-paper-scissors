fcd8b3cf0cc0eb3a65ee41b73c53f4bd7cd2be53
chbphone55 <chbphone55@gmail.com>
2018-08-01 13:59:43 -0400

    fix random number generator

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const moves = [
     name: 'Scissors'
   }
 ]
-const randomMove = () => Math.floor((Math.random() * 100) % moves.length)
+const randomMove = () => Math.floor(Math.random() * moves.length)
 const makeMove = (playerMove, aiMove) => `
   ${moves[playerMove].name} VS ${moves[playerMove.name]}
   ${(playerMove === aiMove ? 'TIE' : moves[playerMove].win === aiMove ? 'WIN' : 'LOSE')}
